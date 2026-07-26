80889655c92b41d31ed005f5728d42d141161ccc
lemon-7799997 <rhs.citrus@gmail.com>
2026-07-26 22:06:06 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 cargo build --release
 cargo zigbuild --release --target x86_64-pc-windows-gnullvm
+
+CMD="cp \"$PWD/target/release/img_tool\" \"$PWD/target/x86_64-pc-windows-gnullvm/release/img_tool.exe\" ."
+echo "$CMD"
+echo "$CMD" | pbcopy
